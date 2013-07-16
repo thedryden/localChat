b3ccf359b5339326824d452f6c4b417932d9bf09
Matthew Dryden <thedryden@hotmail.com>
2013-07-15 17:49:25 -0700

    First Commit

---
+++ localChat/read.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace localChat
+{
+    public partial class read : PhoneApplicationPage
+    {
+        public read()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(
+                new Uri("/write.xaml", UriKind.Relative)
+            );
+        }
+    }
+}
---
+++ localChat/write.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace localChat
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(
+                new Uri("/read.xaml", UriKind.Relative)
+            );
+        }
+    }
+}
